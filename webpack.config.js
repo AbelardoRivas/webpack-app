07b0bd05fbd63a7d79b1431de9ae8c5063009d46
Abelardo Rivas <abelardo.rivas1@gmail.com>
2019-02-14 16:33:59 -0400

    Creando reglas para imágenes

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,10 @@ module.exports = {
                     }
                 ],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
             }
         ]
     }
